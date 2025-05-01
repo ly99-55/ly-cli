8b8d5e6cc6e07f86593c2b8d4b43276197389fe0
ly <ly141563@163.com>
2025-05-01 17:16:46 +0800

    fix: 修改命令

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Command } from 'commander';
 import {version} from '../package.json';
 import { create } from './command/create';
-const program = new Command();
+const program = new Command("ly");
 program.name('ly').description('一个简单的脚手架工具');
 program.version(version, '-v, --version');
 program.command('create').description('创建一个新项目').argument('[name]','项目名称').action(async(dirName?:string) => {
