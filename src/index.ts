d9c2ad29c374d4d898258fbe48abc3ab0428ced5
ly <ly141563@163.com>
2025-05-01 17:39:55 +0800

    fix:优化依赖

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import { Command } from 'commander';
+import {Command} from 'commander';
 import {version} from '../package.json';
-import { create } from './command/create';
-const program = new Command("ly");
-program.name('ly').description('一个简单的脚手架工具');
-program.version(version, '-v, --version');
-program.command('create').description('创建一个新项目').argument('[name]','项目名称').action(async(dirName?:string) => {
-    create(dirName)
-});
+import {create} from './command/create';
+const program = new Command('dawei');
+program.version(version, '-v --version');
+
+program.command('create').description('创建一个新项目').argument('[name]', '项目名称').action(async (dirName) => {
+    create(dirName);
+    // if (dirName) {
+    //     create(dirName);
+    // } else {
+    //     console.log(`create ${dirName}`)
+    // }
+    });
+
 program.parse();
